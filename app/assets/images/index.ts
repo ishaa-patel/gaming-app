4c8b69783c1dfa5773fd87a08ce4dbb877d5d127
Isha <ishapatel2401@gmail.com>
2024-06-13 19:57:22 +0530

    import-images
    import all images in single file

---
+++ app/assets/images/index.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+const images = {
+    introImg: require('./introImg.jpg'),
+    bgImg: require('./bg-img.jpg'),
+    userImg: require('./user.png'),
+    marioImg: require('./super-mario.png'),
+    subwayImg: require('./subway-surf.png'),
+    birdsImg: require('./angry-birds.jpg'),
+    batManImg: require('./batMan.png'),
+    ironManImg: require('./ironMan.jpg'),
+    spiderManImg: require('./spiderMan.jpg'),
+    superManImg: require('./superMan.jpg'),
+    chessImg: require('./chess.png'),
+    cricketImg: require('./cricket.png'),
+    loginImg: require('./login-img.png'),
+    registerImg: require('./register-img.png'),
+}
+export default images;
