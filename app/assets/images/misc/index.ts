969ad1e9e25dfefcc4ede0688c367b54623858a9
Isha <ishapatel2401@gmail.com>
2024-06-13 19:57:51 +0530

    logo-file
    logo file

---
+++ app/assets/images/misc/index.ts
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+const logos = {
+    facebook: require('./facebook.png'),
+    google: require('./google.png'),
+    twitter: require('./twitter.png'),
+}
+export default logos;
