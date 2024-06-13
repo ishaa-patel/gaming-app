bbef24839d643aed22fcaeeb79068ad91144fc5d
Isha <ishapatel2401@gmail.com>
2024-06-13 19:58:27 +0530

    import-components

---
+++ app/components/index.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import BannerSlider from './BannerSlides';
+import CustomSwitch from './CustomSwitch';
+import GamesList from './GamesList';
+import CustomDrawer from './CustomDrawer';
+import CustomInputField from './CustomInputField';
+import CustomButton from './CustomButton';
+export {
+    BannerSlider,
+    CustomSwitch,
+    GamesList,
+    CustomDrawer,
+    CustomInputField,
+    CustomButton,
+};
