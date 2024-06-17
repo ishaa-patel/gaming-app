51ac22d3dabcac07d0f4e6c74c40bfe42f66f8a0
Isha <ishapatel2401@gmail.com>
2024-06-17 17:06:39 +0530

    useEffect and useMemo Hooks

--- app/screens/CartScreen.tsx
+++ app/screens/CartScreen.tsx
@@ -1,7 +1,15 @@
 /* eslint-disable react/react-in-jsx-scope *//* eslint-disable prettier/prettier */
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { View, Text, Button, Alert } from 'react-native';
 import { styles } from './styles/ProfileToSettingStyle';
+
+//useMemo Practice
+const nums = new Array(30_00_000).fill(0).map((_, i) => {
+    return {
+        index: i,
+        isMagical: i === 20_00_000,
+    };
+});
 const CartScreen = () => {
     //useEffect Practice
     const [count, setCount] = useState(0);
@@ -21,8 +29,14 @@ const CartScreen = () => {
     useEffect(() => {
         Alert.alert('Count Updated!');
     }, [count]);
+
+    //useMemo
+    const [numbers, setNumbers] = useState(nums);
+    //const magical = numbers.find(item => item.isMagical === true); // Expensive Computation
+    const magical = useMemo(() => numbers.find(item => item.isMagical === true), [numbers]);
     return (
         <View style={styles.container}>
+            <Text>Magical number is: {magical?.index}</Text>
             <Text style={styles.text}>Count is : {count}</Text>
             <Button title="Count" onPress={() => setCount((count) => count + 1)} />
         </View>
