c0e057f67db4a68c6ddda06a8187d7582d23b087
Isha <ishapatel2401@gmail.com>
2024-06-26 17:21:41 +0530

    Update drawer profile header
    implement redux to update image and username in drawer

--- app/components/CustomDrawer.tsx
+++ app/components/CustomDrawer.tsx
@@ -2,12 +2,13 @@
 import { Text, View, TouchableOpacity, ImageBackground, Image } from 'react-native';
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import { styles } from './styles/CustomDrawerStyle';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import images from '../assets/images';
 import { useNavigation } from '@react-navigation/native';
 import constants from '../constants/AppConstants';
 import { AuthContext } from '../providers/AuthContextProvider';
 import { useContext } from 'react';
+import { connect } from 'react-redux';
 
 const CustomDrawer = (props) => {
     const navigation = useNavigation();
@@ -16,13 +17,12 @@ const CustomDrawer = (props) => {
         <View style={styles.container}>
             <DrawerContentScrollView {...props} contentContainerStyle={styles.contentContainer}>
                 <ImageBackground source={images.bgImg} style={{ padding: 20 }}>
-                    <Image source={images.userImg} style={styles.userimg} />
+                    <Image source={props.displayDrawerImg ? { uri: props.displayDrawerImg } : images.updateProfileImg} style={styles.userimg} />
                     <View style={styles.userdetail}>
-                        <Text style={styles.username}>User Name</Text>
-                        <TouchableOpacity onPress={() => navigation.navigate(constants.NAV_PROFILE)}>
-                            <Text style={styles.updateprofile}>
-                                <Ionicons name="cloud-upload" size={15} />Update Profile
-                            </Text>
+                        <Text style={styles.username}>{props.displayDrawerUserName ? props.displayDrawerUserName : "Welcome! User"}</Text>
+                        <TouchableOpacity style={{ flexDirection: 'row' }} onPress={() => navigation.navigate(constants.NAV_PROFILE)}>
+                            <MaterialIcons name="upload" size={15} />
+                            <Text style={styles.updateProfile}>Update Profile</Text>
                         </TouchableOpacity>
                     </View>
                 </ImageBackground >
@@ -33,11 +33,18 @@ const CustomDrawer = (props) => {
             <View style={styles.separator}>
                 <TouchableOpacity style={styles.footer}
                     onPress={() => { logout(); }}>
-                    <Ionicons name="exit-outline" color="black" size={22} />
+                    <MaterialIcons name="exit-to-app" color="black" size={22} />
                     <Text style={styles.signout}>Sign Out</Text>
                 </TouchableOpacity>
             </View>
         </View >
     );
 };
-export default CustomDrawer;
+
+export const mapStateToProps = (state: any) => {
+    return {
+        displayDrawerImg: state.profile.imgLink,
+        displayDrawerUserName: state.profile.name,
+    };
+};
+export default connect(mapStateToProps)(CustomDrawer);
