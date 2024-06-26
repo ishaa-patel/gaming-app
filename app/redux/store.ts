105e89ae485be1861a419f8c180d950e2b005159
Isha <ishapatel2401@gmail.com>
2024-06-26 17:27:08 +0530

    add profile reducer

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,9 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../redux/counter/CounterSlice';
+import updateProfileReducer from './update_profile/UpdateProfileReducer';
 
 export default configureStore({
     reducer: {
         counter: counterReducer,
+        profile: updateProfileReducer,
     },
 });
