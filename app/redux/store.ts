caec62325f59e161bbcf822d69f9c400b38551fe
Isha <ishapatel2401@gmail.com>
2024-06-27 18:24:55 +0530

    add reducer

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -2,10 +2,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../redux/counter/CounterSlice';
 import updateProfileReducer from './update_profile/UpdateProfileReducer';
+import APISlice from './games_api/API_Reducer';
 
 export default configureStore({
     reducer: {
         counter: counterReducer,
         profile: updateProfileReducer,
+        games: APISlice,
     },
 });
