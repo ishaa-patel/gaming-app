703e72fef5fea39ecf3fd64d95b1afb7b70507f6
Isha <ishapatel2401@gmail.com>
2024-06-27 18:24:43 +0530

    display fetch API data

--- app/screens/SettingScreen.tsx
+++ app/screens/SettingScreen.tsx
@@ -1,7 +1,17 @@
 /* eslint-disable prettier/prettier */
 import { View, Text } from 'react-native';
 import { styles } from './styles/ProfileToSettingStyle';
+import { useDispatch, useSelector } from 'react-redux';
+import { getApi } from '../redux/games_api/API_Actions';
+import { useEffect } from 'react';
 const SettingScreen = () => {
+    const dispatch = useDispatch();
+    const fetchAPI = useSelector((state) => state.games.data);
+    console.log("Result API:", JSON.stringify(fetchAPI));
+
+    useEffect(() => {
+        dispatch(getApi());
+    }, []);
     return (
         <View style={styles.container}>
             <Text style={styles.text}>SettingScreen</Text>
