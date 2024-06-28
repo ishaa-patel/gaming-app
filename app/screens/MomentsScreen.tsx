a45811764c557ceb5a075635204a3219d53b7753
Isha <ishapatel2401@gmail.com>
2024-06-28 14:48:21 +0530

    fetch data using APIContext
    fetch data using API Context from AxiosNetworkProvider

--- app/screens/MomentsScreen.tsx
+++ app/screens/MomentsScreen.tsx
@@ -1,7 +1,23 @@
 /* eslint-disable prettier/prettier */
 import { View, Text } from 'react-native';
 import { styles } from './styles/ProfileToSettingStyle';
+import { useContext, useEffect } from 'react';
+import { APIContext } from '../providers/AxiosNetworkProvider';
+import constants from '../constants/AppConstants';
 const MomentsScreen = () => {
+    const apiInstance = useContext(APIContext);
+    const fetchData = async () => {
+        try {
+            const response = await apiInstance.get(constants.POST_URL);
+            console.log('fetching data:', JSON.stringify(response));
+        }
+        catch (err) {
+            console.log('Error at fetching data:', err);
+        }
+    };
+    useEffect(() => {
+        fetchData();
+    }, []);
     return (
         <View style={styles.container}>
             <Text style={styles.text}>MomentsScreen</Text>
