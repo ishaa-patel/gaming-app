986d5a7e1f3f5d5e5e1eff8c6bccd5ad4794823b
Isha <ishapatel2401@gmail.com>
2024-06-28 16:28:00 +0530

    Update MomentsScreen

--- app/screens/MomentsScreen.tsx
+++ app/screens/MomentsScreen.tsx
@@ -1,15 +1,17 @@
 /* eslint-disable prettier/prettier */
 import { View, Text } from 'react-native';
 import { styles } from './styles/ProfileToSettingStyle';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { APIContext } from '../providers/AxiosNetworkProvider';
 import constants from '../constants/AppConstants';
 const MomentsScreen = () => {
+    const [value, setValue] = useState();
     const apiInstance = useContext(APIContext);
     const fetchData = async () => {
         try {
             const response = await apiInstance.get(constants.POST_URL);
-            console.log('fetching data:', JSON.stringify(response.data));
+            setValue(response.data);
+            // console.log('fetching data:', JSON.stringify(response.data));
         }
         catch (err) {
             console.log('Error at fetching data:', err);
@@ -20,7 +22,9 @@ const MomentsScreen = () => {
     }, []);
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>MomentsScreen</Text>
+            <Text style={[styles.text, { paddingBottom: 30 }]}>Resultant Data:</Text>
+            <Text style={styles.text}>Title: {JSON.stringify(value.title)}</Text>
+            <Text style={styles.text}>Body: {JSON.stringify(value.body)}</Text>
         </View>
     );
 };
