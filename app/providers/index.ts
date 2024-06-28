0e67546ef13d1ffd900185b2c376467320601609
Isha <ishapatel2401@gmail.com>
2024-06-28 14:44:36 +0530

    Create index.ts
    create index.ts to export providers file

---
+++ app/providers/index.ts
@@ -0,0 +1,4 @@
+/* eslint-disable prettier/prettier */
+import { AuthProvider } from './AuthContextProvider';
+import { AxiosNetworkProvider } from './AxiosNetworkProvider';
+export { AuthProvider, AxiosNetworkProvider };
